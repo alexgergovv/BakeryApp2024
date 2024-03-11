d89935d8adb49175550ce1749f9d0c08ffeb645c
alexgergovv <alexgergovv@gmail.com>
2024-03-11 19:10:10 +0200

    ServiceCollectionExtensions added to reduce the code complexity in Program.cs

---
+++ BakeryApp2024/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using BakeryApp2024.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BakeryApp2024.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+            return services;
+        }
+    }
+}
