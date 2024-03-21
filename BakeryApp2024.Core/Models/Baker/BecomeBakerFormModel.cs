84f2477ef50b256eec8823119f07fdf2264f3f82
alexgergovv <alexgergovv@gmail.com>
2024-03-21 21:18:35 +0200

    BecomeBakerModel created

--- BakeryApp2024.Core/Models/Baker/BecomeBakerFormModel.cs
+++ BakeryApp2024.Core/Models/Baker/BecomeBakerFormModel.cs
@@ -1,6 +1,17 @@
-﻿namespace BakeryApp2024.Core.Models.Baker
+﻿using System.ComponentModel.DataAnnotations;
+using static BakeryApp2024.Core.Constants.MessageConstants;
+using static BakeryApp2024.Infrastructure.Constants.DataConstants;
+
+namespace BakeryApp2024.Core.Models.Baker
 {
 	public class BecomeBakerFormModel
 	{
-	}
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(BakerPhoneNumberMaxLength,
+            MinimumLength = BakerPhoneNumberMinLength, 
+            ErrorMessage = LengthMessage)]
+        [Display(Name = "Phone number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
 }
