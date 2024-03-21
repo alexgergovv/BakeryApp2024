d26e4aa57e254e3dc2284238085531452cf629c0
alexgergovv <alexgergovv@gmail.com>
2024-03-21 22:49:13 +0200

    Become an agent implemented

--- BakeryApp2024/Controllers/BakerController.cs
+++ BakeryApp2024/Controllers/BakerController.cs
@@ -1,7 +1,9 @@
-﻿using BakeryApp2024.Core.Contracts;
+﻿using BakeryApp2024.Attributes;
+using BakeryApp2024.Core.Contracts;
 using BakeryApp2024.Core.Models.Baker;
 using BakeryApp2024.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using static BakeryApp2024.Core.Constants.MessageConstants;
 
 namespace BakeryApp2024.Controllers
 {
@@ -15,22 +17,31 @@ namespace BakeryApp2024.Controllers
 				bakerService = _bakerService;
         }
 
-        [HttpGet]
-		public async Task<IActionResult> Become()
+		[HttpGet]
+        [NotABaker]
+		public IActionResult Become()
 		{
-			if (await bakerService.ExistsByIdAsync(User.Id()))
-			{
-				return BadRequest();
-			}
-
 			var model = new BecomeBakerFormModel();
 
 			return View(model);
 		}
 
 		[HttpPost]
+		[NotABaker]
 		public async Task<IActionResult> Become(BecomeBakerFormModel model)
 		{
+			if (await bakerService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
+			{
+				ModelState.AddModelError(nameof(model.PhoneNumber), PhoneExists);
+			}
+
+            if (ModelState.IsValid == false)
+			{
+				return View(model);
+			}
+
+			await bakerService.CreateAsync(User.Id(), model.Name, model.PhoneNumber);
+
 			return RedirectToAction(nameof(ProductController.All), "Product");
 		}
 	}
