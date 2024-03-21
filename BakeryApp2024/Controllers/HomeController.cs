bdbffc345ca399b6d246613adcf928244135fb52
alexgergovv <alexgergovv@gmail.com>
2024-03-21 19:58:31 +0200

    Home page implemented

--- BakeryApp2024/Controllers/HomeController.cs
+++ BakeryApp2024/Controllers/HomeController.cs
@@ -1,24 +1,27 @@
-﻿//using BakeryApp2024.Core.Models.Home;
+﻿using BakeryApp2024.Core.Contracts;
 using BakeryApp2024.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BakeryApp2024.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IProductService _productService)
         {
             _logger = logger;
+            productService = _productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            //var model = new IndexViewModel();
-            //return View(model);
-            return View();
+            var model = await productService.GetThreeProducts();
+
+            return View(model);
         }
 
 
