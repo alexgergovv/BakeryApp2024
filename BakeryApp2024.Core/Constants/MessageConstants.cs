aab959e35965be493946988780e7482404b3f0be
alexgergovv <alexgergovv@gmail.com>
2024-03-21 21:48:49 +0200

    NotABaker attribute created

--- BakeryApp2024.Core/Constants/MessageConstants.cs
+++ BakeryApp2024.Core/Constants/MessageConstants.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BakeryApp2024.Core.Constants
+﻿namespace BakeryApp2024.Core.Constants
 {
     public class MessageConstants
     {
         public const string RequiredMessage = "The {0} field is required";
+
         public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone number already exists. Enter another one";
     }
 }
---
+++ BakeryApp2024/Attributes/NotABakerAttribute.cs
@@ -0,0 +1,28 @@
+﻿using BakeryApp2024.Core.Contracts;
+using BakeryApp2024.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BakeryApp2024.Attributes
+{
+    public class NotABakerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IBakerService? bakerService = context.HttpContext.RequestServices.GetService<IBakerService>();
+
+            if (bakerService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (bakerService != null
+                && bakerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
